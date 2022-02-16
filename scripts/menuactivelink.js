bcc723958a58c34ca7c8989909b3dbda5a0782d1
batoxa <nbatorin@gmail.com>
2022-02-17 00:35:53 +0300

    title background

--- scripts/menuactivelink.js
+++ scripts/menuactivelink.js
@@ -2,26 +2,26 @@ const sections = document.querySelectorAll(".section");
 const links = document.querySelectorAll(".menu-item");
 
 const callback = (entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting && entry.intersectionRatio > 0.6) {
-      links.forEach((link) => link.classList.remove("activeLink"));
+    entries.forEach((entry) => {
+        if (entry.isIntersecting && entry.intersectionRatio > 0.6) {
+            links.forEach((link) => link.classList.remove("activeLink"));
 
-      const activeId = entry.target.id;
-      const activeLink = document.querySelector(
-        `.menu-item[href="#${activeId}"]`
-      );
+            const activeId = entry.target.id;
+            const activeLink = document.querySelector(
+                `.menu-item[href="#${activeId}"]`
+            );
 
-      if (activeLink) {
-        activeLink.classList.add("activeLink");
-      }
-    }
-  });
+            if (activeLink) {
+                activeLink.classList.add("activeLink");
+            }
+        }
+    });
 };
 
 const sectionObserver = new IntersectionObserver(callback, {
-  threshold: [0.2, 0.5, 0.8]
+    threshold: [0.1, 0.3, 0.5, 0.7, 0.9]
 });
 
 sections.forEach((section) => {
-  sectionObserver.observe(section);
+    sectionObserver.observe(section);
 });
