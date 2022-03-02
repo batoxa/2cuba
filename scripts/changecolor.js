ed1a58c3e269b73015a74c06a48f56c47e1e6ad7
batoxa <nbatorin@gmail.com>
2022-03-02 15:23:32 +0300

    add mobile menu icon

--- scripts/changecolor.js
+++ scripts/changecolor.js
@@ -4,18 +4,3 @@ function changeColor(color) {
         el.style.color = color;
     });
 }
-
-// function clickRed() {
-//     const switchColorItems = [...document.getElementsByClassName("main-color")];
-//     switchColorItems.forEach((el) => {
-//         el.classList.add("main-color-red");
-//         el.classList.remove("main-color-blue");
-//     });
-// }
-// function clickRed() {
-//     const switchColorItems = [...document.getElementsByClassName("main-color")];
-//     console.log(switchColorItems.length);
-//     switchColorItems.forEach((el) => {
-//         el.style = "background-color: darkred";
-//     });
-// }
